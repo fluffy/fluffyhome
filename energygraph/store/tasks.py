69b062d012cb6ef4aa4176e6d68d642f1d9cec0c
Cullen Jennings <fluffy@iii.ca>
2013-04-20 21:44:41 -0600

    celery tasks are runngin

--- energygraph/store/tasks.py
+++ energygraph/store/tasks.py
@@ -1,6 +1,8 @@
 import logging
 import time
-
+from celery.task.schedules import crontab  
+from celery.decorators import periodic_task  
+  
 from celery.task import task
 
 from energygraph.store.views import *
@@ -22,3 +24,8 @@ def updateHourly(): # /tasks/update/*/*/*/
 def updateWindAB4(): #  /tasks/pollWindAB4/ab-52/
     pass
 
+
+# this will run every minute
+@periodic_task(run_every=crontab(hour="*", minute="*", day_of_week="*"))  
+def testTaskOne():      
+    print "firing test task"         
