b1904cd57bc0ceb83b62a5d28b78d8471f8475dd
Cullen Jennings <fluffy@iii.ca>
2013-04-28 16:35:05 -0600

    update taks names

--- energygraph/store/tasks.py
+++ energygraph/store/tasks.py
@@ -12,13 +12,14 @@ logger = logging.getLogger('energygraph')
 
 @periodic_task(run_every=crontab(hour="*", minute="25,55", day_of_week="*"))  
 def taskUpdateHourly(): # /tasks/update/*/*/*/
-    logger.info( "Running task updateHourly" )
+    logger.info( "Running task taskUpdateHourly" )
     doUpdateAllValuesNow()
     
 
 #@periodic_task(run_every=crontab(minute="*/5")) # every 5 minutes
 def taskShowAllWindSensors(): # refresh the wind cache 
-     doShowAllWindSensors()
+    logger.info( "Running task taskShowAllWindSensors" )
+    doShowAllWindSensors()
 
 
 #@periodic_task(run_every=crontab(minute="*/10")) # every 5 minutes
@@ -29,20 +30,24 @@ def taskShowAllWindSensors(): # refresh the wind cache
 
 @periodic_task(run_every=crontab(minute="*/5")) # every 5 minutes
 def taskPollKiteRanch(): # refresh the wind cache 
-     doPollWindAB3( 'highasakite', None )  
+    logger.info( "Running taskPollKiteRanch " )
+    doPollWindAB3( 'highasakite', None )  
 
 
 @periodic_task( run_every=crontab(minute="*/10") ) 
 def taskPollCalgaryAirport(): # refresh the wind cache 
-     doPollWindAB4( 'ab-52', None )  
+    logger.info( "Running taskPollCalgaryAirport" )
+    doPollWindAB4( 'ab-52', None )  
 
 
 @periodic_task( run_every=crontab(minute="*/10") ) 
 def taskPollRedDeerAirport(): # refresh the wind cache 
-     doPollWindAB4( 'ab-29', None )  
+    logger.info( "Running task taskPollRedDeerAirport" )
+    doPollWindAB4( 'ab-29', None )  
 
 
 @periodic_task( run_every=crontab(minute="*/10") ) 
 def taskPollLethbrideAirport(): # refresh the wind cache 
-     doPollWindAB4( 'ab-30', None )  
+    logger.info( "Running task taskPollLethbrideAirport" )
+    doPollWindAB4( 'ab-30', None )  
 
