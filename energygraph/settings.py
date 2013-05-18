b480b090853af2b0d57e8d2f4da113f548f82563
Cullen Jennings <fluffy@iii.ca>
2013-05-18 10:18:17 -0600

    changed DB name

--- energygraph/settings.py
+++ energygraph/settings.py
@@ -30,7 +30,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'hero',                      # Or path to database file if using sqlite3.
+        'NAME': 'energygraph',                      # Or path to database file if using sqlite3.
         'USER': 'fluffy',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
