95cc24f7f4b615592a9bef11590a656f80a85a7d
Cullen Jennings <fluffy@iii.ca>
2013-05-20 15:45:15 -0600

    update from build

--- ecm1240/fabfile.py
+++ ecm1240/fabfile.py
@@ -4,6 +4,8 @@ from fabric.api import *
 @task
 @hosts('fluffymini50.fluffyhome.com')
 def deploy():
+    local( "git commit --allow-empty -a -m 'update from build' " );
+    local( "git push" );
     run( "cd ~/src/fluffyhome; git pull " );
     run( "cd ~/src/fluffyhome/ecm1240; make clean ; make  " );
     run( "cd ~/src/fluffyhome/ecm1240; sudo make install-fluffy " );
